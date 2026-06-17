1d9e6119a496059c45f1c73348d334a07a2b0a34
Marco Ris <marco.ris@risdesign.ch>
2026-06-17 11:03:58 +0200

    experimental/caesarCipherPlus.py erstellen

---
+++ hacking_secret_ciphers/experimental/caesarCipherPlus.py
@@ -0,0 +1,3 @@
+import sys, pyperclip, cryptomath, random
+  5 . SYMBOLS = """ !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]
+ ^_`abcdefghijklmnopqrstuvwxyz{|}~"""  # note the space at the front
